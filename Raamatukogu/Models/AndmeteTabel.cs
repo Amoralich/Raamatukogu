fbad37c114eebd53396becb142fd28cfecccdd69
opilane <opilane@HP-CZC6297B9N>
2020-01-17 11:20:57 +0200

    asdasd

---
+++ Raamatukogu/Models/AndmeteTabel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Raamatukogu.Models
+{
+	public class AndmeteTabel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Author { get; set; }
+		public string rentalCube  {get; set;}
+		public string TheLengthOfTheLoan { get; set; }
+		public string availability { get; set; }
+	}
+}
